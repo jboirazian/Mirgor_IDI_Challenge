35c9ff25cd955b5655d60011906b040d70ec3c8b
Juan Boirazian <jboirazian@frba.utn.edu.ar>
2021-12-07 19:12:39 -0300

    -
    Agregado de Multiples procesos y arreglando la API del Cloud server

--- Cloud_server.py
+++ Cloud_server.py
@@ -4,16 +4,28 @@ import pandas as pd
 import numpy as np
 import json
 
+CPU_MAX_TEMP=35 #### TEMPERATURA EN `C ,EN LA CUAL EL SERVIDOR CLOUD ENVIA UNA RESPUESTA SI LA TEMPERATURA ENVIADA ES MAYOR
+
 app = Flask('Mirgor_CLOUD_SERVER')
 
-# Definimos un nuevo endoint o resource path (prueba_get) y explicitamos el método.
 @app.route("/api_cloud",methods=['POST'])
-def pruebaGet():
+def POST_CLOUD():
     data = request.get_json(force=True)
-    data['p1']+=2
-    data['p2']-=2
-    data['p3']=data['p3']*2
-    data['pN']=data['pN']/2
+    #### VERIFICAMOS QUE ESTEN LOS DATOS DE LAS QUERIES , DE EXISTIR HACEMOS UNA ACCION CON ESTOS:
+    if 'p1' in data:
+        if (data['p1']>35):
+            data['p1']="CPU_TEMP_HIGH"#### LA TEMEPERATURA DEL CPU ES ALTA
+        else:
+            data['p1']="CPU_TEMP_OK" #### LA TEMPERATURA DEL CPU ESTA EN RANGOS ACEPTABLES
+    if 'p2' in data:#### HAGO DISTINTAS OPERACIONES CON LOS PROCESOS RECIBIDOS
+        data['p2']-=2
+    if 'p3' in data:
+        data['p3']=data['p3']*2
+    if 'p4' in data:
+        data['p4']=data['p4']*3
+    if 'p5' in data:
+        data['p5']=data['p5']+5
+    
     
     return data
     
--- PC.py
+++ PC.py
@@ -1,16 +1,65 @@
 from random import seed
 from random import randint
 import time
-from pyspectator.processor import Cpu
 from time import sleep
 import requests
-
+import multiprocessing
+import json
 url = 'http://localhost:5003/api_cloud'
-cpu = Cpu(monitoring_latency=1)
-while True:
-    sleep(1)
-    seed(time.time())
-    datos={'p1' : cpu.temperature,'p2' : randint(0,100),'p3' : randint(0,50) , 'pN':randint(0,120)}
-    r = requests.post(url,json=datos)
+core_temp=27
+
+N_PROCESOS=10 #### Cantidad de procesos
+
+def Proceso_CPU_Temp(ID):
+	while True:
+		global core_temp
+		print(f'Iniciando proceso de actualizacion de temperatura del CPU')
+		time.sleep(1)
+		core_temp+=1 #### TEST PARA VER COMO CAMBIA LA TEMPERATURA
+		print(f'Temperatura actualizada , CPU TEMP ACTUAL: {core_temp}')
+		##### PREPARO LOS DATOS Y LOS ENVÍO
+		nombre_proceso="p"+str(ID)
+		valor_proceso=core_temp
+		r = requests.post(url,json={nombre_proceso:(valor_proceso)})
+		print(f'Respuesta del servidor Cloud :{r.json()}')
+		#####
+
     
-    print(r.json())
+def Proceso_generico(ID,tiempo): 
+	###PROCESO GENERICO QUE REALIZA UN SLEEP DEPENDIENDO DE LA VARIABLE "tiempo".
+	###una vez que termina envía una request tipo POST al servidor cloud con el formato {pn:tiempo}
+	while True:
+		print(f'Iniciando proceso que demora {tiempo} segundos')
+		time.sleep(tiempo)
+		print(f'Proceso que demoró {tiempo} segundos terminado')
+		##### PREPARO LOS DATOS Y LOS ENVÍO
+		nombre_proceso="p"+str(ID)
+		valor_proceso=tiempo
+		#####
+		print(f'Respuesta enviada al servidor Cloud :{nombre_proceso} : {valor_proceso}')
+		r = requests.post(url,json={nombre_proceso:(valor_proceso)})
+		print(f'Respuesta del servidor Cloud :{r.json()}')
+    
+    
+seed(123) ### dejo la seed fija para que sea mas facil ver un patrón
+tiempos=[]
+for i in range(N_PROCESOS):
+    tiempos.append(randint(1,3))
+
+def main():
+	procesos=[]
+	proceso_temp=  multiprocessing.Process(target=Proceso_CPU_Temp,args=[1])
+	proceso_temp.start()
+	for z in range(2,N_PROCESOS): #### INICIO LOS PROCESOS GENERICOS 
+		pro=  multiprocessing.Process(target=Proceso_generico,args=(z+1,tiempos[z]),)
+		pro.start()
+		print(f"PROCESO {z} INICIADO")
+		procesos.append(pro)
+		z+=1
+	for proceso in procesos:
+		proceso.join()
+
+
+
+if __name__ == "__main__":
+    main()
